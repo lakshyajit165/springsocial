677739021b342df2814c77cb247d87dc67102251
Lakshyajit Laxmikant <lakshyajit165@gmail.com>
2024-06-29 13:05:20 +0530

    added profile avatar in case the profile image url is not available

--- spring_social_client/src/app/components/profile/profile.component.ts
+++ spring_social_client/src/app/components/profile/profile.component.ts
@@ -8,34 +8,40 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor(
-    private _authService: AuthService,
-    private _router: Router
-  ) { }
+  constructor(private _authService: AuthService, private _router: Router) {}
 
   ngOnInit(): void {
     this._authService.getUserDetails().subscribe({
       next: (data: IUser) => {
         this.name = data.name;
         this.imageUrl = data.imageUrl;
+        this.setInitials(this.name);
       },
-      error: err => {
+      error: (err) => {
         console.log(err);
-      }
-    })
+      },
+    });
   }
 
-  public name: string = "";
-  public imageUrl: string = "";
+  public name: string = '';
+  public imageUrl: string = '';
+  public initials: string = '';
   faSignOut = faSignOut;
+
+  setInitials(name: string): void {
+    let names = name.split(' ');
+    if (names.length >= 2) {
+      this.initials = names[0][0].toUpperCase() + names[1][0].toUpperCase();
+    } else {
+      this.initials = names[0][0].toUpperCase();
+    }
+  }
 
   logout(): void {
     this._authService.logout();
-    this._router.navigate(["/login"]);
+    this._router.navigate(['/login']);
   }
-
 }
